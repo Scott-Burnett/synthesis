4340d6c265bd3bee0758d6f9eb6d6686c79abf71
Scott Burnett <G14B4098@ict.ru.ac.za>
2019-03-07 15:29:39 +0200

    work in prac

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,34 +1,54 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar n = n > 12 && n < 3097 && n % 12 = 0
 
-let area _ _ =
-    failwith "Not implemented"
+let area a b = match a >= 0.0 && b >= 0.0 with
+    |true -> a * b * 0.5
+    |_ -> failwith "either base or height is negative"
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo n = match n < 0 with 
+    |true -> n * -1
+    |_ -> n * 2
 
-let min _ _ =
-    failwith "Not implemented"
+let min a b  = match a < b with 
+    |true -> a
+    |_ -> b
 
-let max _ _ =
-    failwith "Not implemented"
+let max a b = match a > b with 
+    |true -> a
+    |_ -> b
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime h m s = (h * 3600) + (m * 60) + s
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime t = match t >= 0 with
+    |true ->    let m = t / 60 in   
+                    let s = t % 60 in
+                        let h = m / 60 in
+                            let m = m % 60 in 
+                                h,m,s
+    |_ -> 0,0,0
 
-let digits _ =
-    failwith "Not implemented"
+let digits n = let rec numdigits a c =
+                   match (a / 10) = 0 with
+                    |true -> c
+                    |_ -> numdigits (a / 10) (c + 1) 
+               match (n < 10 && n > -10) with 
+                |true -> 1
+                |_ -> numdigits n 1
 
 let minmax _ =
     failwith "Not implemented"
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap y = match y < 1582 with 
+                |true -> failwith "Year is less than 1582"
+                |_ -> match y % 100 = 0 with 
+                    |true -> match y % 400 = 0 with
+                        |true -> true
+                        |_ -> false
+                    |false -> (y % 4) = 0
+
+                    
+    
 
 let month _ =
     failwith "Not implemented"
