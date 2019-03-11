bfa798798489fd3fbc00eb9124cf83368e464824
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2019-03-11 13:38:48 +0200

    Finished

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -36,19 +36,21 @@ let digits n = let rec numdigits a c =
                 |true -> 1
                 |_ -> numdigits n 1
 
-let minmax _ =
-    failwith "Not implemented"
+let minmax (a,b,c,d) = 
+    let rec func (a,b,c,d) =
+        match a <= b && a <= c && a <= d  with
+            |true -> match d >= a && d >= b && d >= c with 
+                |true -> (a, d)
+                |false -> func (a, c, d, b) 
+            |false -> func (b, c, d, a)
+    func (a,b,c,d)
 
 let isLeap y = match y < 1582 with 
                 |true -> failwith "Year is less than 1582"
-                |_ -> match y % 100 = 0 with 
-                    |true -> match y % 400 = 0 with
-                        |true -> true
-                        |_ -> false
-                    |false -> (y % 4) = 0
-
-                    
-    
+                |_ -> match y % 100 = 0, y % 400 = 0, y % 4 = 0 with 
+                    |true, true, true -> true
+                    |false, _ , true -> true
+                    |_ -> false  
 
 let month m = match m with
                 |1 -> "January", 31
@@ -65,14 +67,47 @@ let month m = match m with
                 |12 -> "December", 31
                 |_ -> failwith "Invalid input"
 
-let toBinary _ =
-    failwith "Not implemented"
+let toBinary n = let rec func n s = 
+                         match n = 0, s = "" with 
+                            |true, true -> "0"
+                            |true, false -> s
+                            |_, _ -> match (n % 2) = 0 with 
+                                |true -> func (n / 2) ("0" + s)
+                                |false -> func (n / 2) ("1" + s)
+                 match n < 0 with 
+                    |true -> failwith "Negative input"
+                    |_ -> func n ""
 
-let bizFuzz _ =
-    failwith "Not implemented"
+let bizFuzz n = let rec func i (a,b,c) = 
+                    match i > n,  (i % 3) = 0, (i % 5) = 0 with 
+                        |true, _, _ -> (a,b,c)
+                        |false, true, true -> func (i + 1) (a + 1, b + 1, c + 1)
+                        |false, true, false -> func (i + 1) (a + 1, b, c)
+                        |false, false, true -> func (i + 1) (a, b + 1, c)
+                        |_, _, _ -> func (i + 1) (a,b,c)
+                func 1 (0,0,0)
+   
+let monthDay d y = 
+    let f = match isLeap y with 
+        |true -> 29
+        |false -> 28
+    let monthdays = (31, f, 31, 30, 31, 30, 31, 31, 30, 31,30, 31)
+    let monthstrings = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+    let rec func n (djan, dfeb, dmar, dapr, dmay, djun, djul, daug, dsep, doct, dnov, ddec) (sjan, sfeb, smar, sapr, smay, sjun, sjul, saug, ssep, soct, snov, sdec) = 
+        match djan = -1 || n < 1 with 
+            |true -> failwith "Out of range"
+            |false -> match (n <= djan)with
+                        |true  -> sjan
+                        |false -> func (n - djan) (dfeb, dmar, dapr, dmay, djun, djul, daug, dsep, doct, dnov, ddec, -1) (sfeb, smar, sapr, smay, sjun, sjul, saug, ssep, soct, snov, sdec, sjan)
+    func d monthdays monthstrings
 
-let monthDay _ _ =
-    failwith "Not implemented"
-
-let coord _ =
-    failwith "Not implemented"
+let coord (x, y) = 
+    let sqrt n = 
+        let rec calc guess lastguess = 
+            match -0.00000001 < (lastguess - guess) && (lastguess - guess) < 0.0000001 with 
+                |true -> guess
+                |_ -> calc ((guess + n / guess) / 2.0) (guess)
+        calc (n / 2.0) 0.0
+    let distance (a, b) = sqrt ((a - x) * (a - x) + (b - y) * (b - y))
+    let within (j, k) w h = ((x > j) && (x < (j + w * 1.0)) && (y < k) && (y > (k - h * 1.0)))
+    (distance, within)    
